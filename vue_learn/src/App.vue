416cfbc3b9e5a6ef12236ed5032d09fe39ea91df
flyjay <545198523@qq.com>
2022-01-28 17:22:28 +0800

    6

--- vue_learn/src/App.vue
+++ vue_learn/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <h1>我是个测试</h1>
-    <MyStudent/>
+    <h1 ref="r1">我是个测试</h1>
+    <title ref="title">我是个ttle</title>
+    <button @click="showRef">按钮</button>
+    <MyStudent ref="stu"/>
   </div>
 </template>
 
@@ -12,6 +14,14 @@ export default {
   components: {MyStudent},
   data() {
     return {};
+  },
+  methods: {
+    showRef()
+    {
+      console.log(this.$refs.r1)// //真实DOM元素
+      console.log(this.$refs.title)//真实DOM元素
+      console.log(this.$refs.stu)//School组件的实例对象（vc）
+    }
   },
 };
 </script>
