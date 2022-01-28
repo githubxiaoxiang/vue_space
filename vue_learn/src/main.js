7070ccc7e980b898b2fbb4d7988d0123df74bfd1
flyjay <545198523@qq.com>
2022-01-28 15:15:35 +0800

    vue inspect >output.js  生成底层脚手架的配置内容 修改output内容没用 只是纯展示输出查看

--- vue_learn/src/main.js
+++ vue_learn/src/main.js
@@ -14,6 +14,8 @@ import router from './router'
 		2.因为vue.runtime.xxx.js没有模板解析器，所以不能使用template配置项，需要使用
 			render函数接收到的createElement函数去指定具体内容。
 */
+
+// vue inspect >output.js  生成底层脚手架的配置内容 修改output内容没用 只是纯展示输出查看
 Vue.config.productionTip = false
 
 new Vue({
