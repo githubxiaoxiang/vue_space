d638ad9ce202bf294f4ea554bf02da46631d22ef
flyjay <545198523@qq.com>
2022-02-10 20:08:18 +0800

    嵌套（二级）路由

---
+++ vue_learn/014_router嵌套使用/pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <!--这里需要写完整路径  -->
+    <router-link class="mytab" active-class="mytab mytabActive" to="/home/message">Message</router-link>
+    <router-link class="mytab" active-class="mytab mytabActive" to="/home/news">News</router-link>
+    <div>
+        <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  mounted() {
+    console.log("Home挂载了");
+    console.log(this);
+    window.homeRoute = this.$route;
+    window.homeRouter = this.$router;
+  },
+  beforeDestroy() {
+    console.log("Home销毁了");
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue_learn/014_router嵌套使用/pages/Message.vue
@@ -0,0 +1,13 @@
+<template>
+    <span>我是message信息</span>
+</template>
+
+<script>
+export default {
+    name:"Message"
+}
+</script>
+
+<style>
+
+</style>
