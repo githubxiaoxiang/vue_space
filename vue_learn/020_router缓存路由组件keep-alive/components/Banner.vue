1d602607e69bf0c27746482e1bc74530f12cdc52
flyjay <545198523@qq.com>
2022-02-11 14:41:18 +0800

    router 缓存路由组件  keep-alive

---
+++ vue_learn/020_router缓存路由组件keep-alive/components/Banner.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Vue Router Demo</h1>
+    <button @click="onBack">后退</button>
+    <button @click="onForward">前进</button>
+    <button @click="onGo">go使用</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Banner",
+  methods: {
+    onBack() {
+      this.$router.back();
+    },
+    onForward() {
+      this.$router.forward();
+    },
+    onGo() {
+      // 正数：前进n步 负数：后退n步
+      this.$router.go(1);
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  margin-left: 10px;
+}
+</style>
---
+++ vue_learn/020_router缓存路由组件keep-alive/pages/News.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <ul>
+      <li>新闻1 
+        <input type="text" name="" id="">
+      </li>
+      <li>新闻2</li>
+      <li>新闻3</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "News",
+  beforeDestroy()
+  {
+    console.log("News组件被销毁")
+  }
+};
+</script>
+
+<style>
+</style>
