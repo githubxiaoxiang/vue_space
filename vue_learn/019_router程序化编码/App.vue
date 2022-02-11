3b8a3474fdf97abf584ac4366e1328e6b660588b
flyjay <545198523@qq.com>
2022-02-11 14:20:13 +0800

    this.$router.push/replace/back/go/forward程序化编码

---
+++ vue_learn/019_router程序化编码/App.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+  <h1>app</h1>
+  <Banner></Banner>
+  	<!-- Vue中借助router-link标签实现路由的切换 -->
+  <router-link class="mytab" active-class="mytab mytabActive" to="/about">About</router-link>
+  <router-link class="mytab" active-class="mytab mytabActive" to="/home">Home</router-link>
+  <div>
+    	<!-- 指定组件的呈现位置 -->
+    <router-view></router-view>
+  </div>
+</div>
+</template>
+
+<script>
+import Banner from './components/Banner';
+    export default {
+        name: "App",
+        components:{
+            Banner
+        }
+    };
+</script>
+<style>
+  /* a{
+    text-decoration:none;
+  } */
+  .mytab{
+    color: blue;
+    margin-left: 20px;
+  }
+  .mytabActive{
+    color: pink;
+  }
+</style>
