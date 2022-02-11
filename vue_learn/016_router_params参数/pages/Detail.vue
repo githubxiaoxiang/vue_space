5012662a5c285dfea389f8f538a034730e4254e5
flyjay <545198523@qq.com>
2022-02-11 10:38:57 +0800

    router params参数

---
+++ vue_learn/016_router_params参数/pages/Detail.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- params -->
+    <h3>id:{{$route.params.id}}</h3>
+    <h3>title:{{$route.params.title}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Detail",
+  mounted()
+  {
+      console.log(this.$route)
+  }
+};
+</script>
+
+<style>
+</style>
