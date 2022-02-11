9d20b81a7574ce7f6d7cbb9a24a1b70a750654f7
flyjay <545198523@qq.com>
2022-02-11 10:29:41 +0800

    router路由query传参和name命名路由

---
+++ vue_learn/015_router query传参和命名路由/pages/Detail.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>id:{{$route.query.id}}</h3>
+    <h3>title:{{$route.query.title}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Detail",
+  mounted()
+  {
+      console.log(this.$route)
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ vue_learn/015_router query传参和命名路由/router/index.js
@@ -0,0 +1,29 @@
+// 该文件专门用于创建整个应用的路由器
+import VueRouter from "vue-router";
+// 引入组件
+import About from "../pages/About"
+import Home from "../pages/Home"
+import Message from "../pages/Message"
+import News from "../pages/News"
+import Detail from "../pages/Detail"
+export default new VueRouter({
+    routes: [{
+        path: '/about',
+        component: About
+    }, {
+        path: '/home',
+        component: Home,
+        children: [{
+            path: "message", //此处一定不要写成 '/message'
+            component: Message,
+            children: [{
+                name: "detail",
+                path: "detail",
+                component: Detail
+            }]
+        }, {
+            path: "news",
+            component: News
+        }]
+    }]
+})
